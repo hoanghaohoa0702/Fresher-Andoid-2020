458b54437cdbf3a13aa34fb997e5646e87ac3a75
HOANG <hoanghaohoa0702@gmail.com>
2022-01-11 16:31:31 +0700

    add button

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            Toast.makeText(applicationContext,"Xin chào",Toast.LENGTH_SHORT).show()
+        }
     }
 }
